7ab634f806c1fc10af4093ec5dc5c08ebb881cfb
anne <richardson.ae@gmail.com>
2017-08-10 15:29:41 -0500

    Add example output from times api

--- apis/times_api.js
+++ apis/times_api.js
@@ -40,3 +40,23 @@ const times = new TheTimes(TIMES_API_KEY)
 times.mostEmailed(function(data) {
   console.log(data[0])
 })
+
+times.mostEmailed(function(articles) {
+  console.log("Most Emailed")
+  console.log("=============")
+  articles.forEach(article => { console.log(article.title) })
+})
+
+times.mostViewed(function(articles) {
+  console.log(" ")
+  console.log("Most Viewed")
+  console.log("=============")
+  articles.forEach(article => { console.log(article.title) })
+})
+
+times.mostShared(function(articles) {
+  console.log(" ")
+  console.log("Most Shared")
+  console.log("=============")
+  articles.forEach(article => { console.log(article.title) })
+})
